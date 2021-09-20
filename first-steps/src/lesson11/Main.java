5b81ed174dcd4c1dac4becdcbbb8e0288bec53c7
Konstantin Kasachev <k.kasachev@gmail.com>
2021-09-20 23:37:41 +0300

    Android Java Core | Lesson 11 | 2 - convert array to arrayList

--- first-steps/src/lesson11/Main.java
+++ first-steps/src/lesson11/Main.java
@@ -1,6 +1,7 @@
 package lesson11;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
@@ -8,9 +9,19 @@ public class Main {
         System.out.println("Hello!");
 
 
-        ArrayOps<String> array1 = new ArrayOps<>("1", "2", "3", "4", "5");
-        array1.swapElements(1, 3);
-        System.out.println(Arrays.toString(array1.getArray()));
+        String[] arr1 = {"1", "2", "3", "4", "5"};
+        Integer[] arr2 = {11, 12, 13, 14, 15};
+
+        ArrayOps<String> array1 = new ArrayOps<>(arr1);
+        array1.swapElements(1, 2);
+        System.out.println("1. String array: " + Arrays.toString(array1.getArray()));
+
+        ArrayOps<Integer> array2 = new ArrayOps<>(arr2);
+        array2.swapElements(1, 4);
+        System.out.println("2. Int array: " + Arrays.toString(array2.getArray()));
+
+        List<Integer> listInt = array2.getListFromArray();
+        System.out.println("3. List from array: " + array2.getArray().getClass().getName() + " to " + listInt.getClass().getName());
 
     }
 
