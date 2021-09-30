0036b3e1d7c0788c21fbda21645ed1bacd9b4a17
Konstantin Kasachev <k.kasachev@gmail.com>
2021-09-30 23:42:20 +0300

    Added CalendarView and switching from main activity to inputs activity

--- FirstAndroidApp/app/src/main/java/com/example/firstandroidapp/MainActivity.java
+++ FirstAndroidApp/app/src/main/java/com/example/firstandroidapp/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.firstandroidapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_inputs);
+        setContentView(R.layout.activity_main);
+
+        Button changeButton1 = findViewById(R.id.switch_to_inputs_button);
+
+
+        changeButton1.setOnClickListener(view1 -> setContentView(R.layout.activity_inputs));
+
 
     }
 }
