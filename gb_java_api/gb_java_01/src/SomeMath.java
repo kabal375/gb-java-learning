6d5a9ecbea0e52de7638f8304ed78574064a606d
Konstantin Kasachev <k.kasachev@gmail.com>
2023-02-15 17:22:08 +0300

    Task2 - Simple numbers

--- gb_java_api/gb_java_01/src/SomeMath.java
+++ gb_java_api/gb_java_01/src/SomeMath.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class SomeMath {
     public static void main(String[] args) {
 
         int n = 4;
         System.out.println("Треугольное число: " + triangleNumber(n));
         System.out.println("Факториал: " + factorial(n));
+
+        System.out.println(simpleNumbers(1000));
 
     }
 
@@ -23,6 +27,30 @@ public class SomeMath {
         } else {
             return n * factorial(n - 1);
         }
+    }
+
+    public static ArrayList<Integer> simpleNumbers(int n) {
+        // Вывести простые числа от 1 до 1000
+
+        ArrayList<Integer> simpleNumbers = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            if (isItSimple(i)) simpleNumbers.add(i);
+
+        }
+
+        return simpleNumbers;
+    }
+
+    public static boolean isItSimple(int n) {
+        if (n <= 1) return false;
+        else if (n <= 3) return true;
+        else if (n % 2 == 0 || n % 3 == 0) return false;
+        int m = 5;
+        while (m * m <= n) {
+            if (n % m == 0 || n % (m + 2) == 0) return false;
+            m += 6;
+        }
+        return true;
     }
 
 }
