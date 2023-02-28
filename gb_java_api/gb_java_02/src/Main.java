7adfa0f355d0121b53734897787817c69dd38b17
Konstantin Kasachev <32961316+kabal375@users.noreply.github.com>
2023-02-28 19:22:29 +0300

    HW4 - custom Queue, reverting queue
    Co-authored-by: Konstantin Kasachev <k.kasachev@yadro.com>

--- gb_java_api/gb_java_02/src/Main.java
+++ gb_java_api/gb_java_02/src/Main.java
@@ -1,16 +1,32 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("ololo"));
+//        System.out.println(isPalindrome("ololo"));
 
 //        writeTest2File();
 
-        System.out.println(Arrays.toString(getDirContent()));
+//        System.out.println(Arrays.toString(getDirContent()));
 
-        write2File(getDirContent());
+//        write2File(getDirContent());
+        List<Integer> myList = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7));
+        IntQueue myQueue = new IntQueue(myList);
+//        myQueue.enqueue(5);
+//        myQueue.enqueue(1);
+//        myQueue.enqueue(2);
+        myQueue.reverse();
+        System.out.println("Sum of elements: " + myQueue.sum());
+        System.out.println(myQueue.first());
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+        System.out.println(myQueue.dequeue());
+
 
     }
 
