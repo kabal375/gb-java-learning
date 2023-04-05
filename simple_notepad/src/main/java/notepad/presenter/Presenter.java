22dfa370b74d0ea3eb37b0eee39bf05a1b536883
Konstantin Kasachev <k.kasachev@yadro.com>
2023-04-05 10:51:44 +0300

    Added logger

--- simple_notepad/src/main/java/notepad/presenter/Presenter.java
+++ simple_notepad/src/main/java/notepad/presenter/Presenter.java
@@ -3,18 +3,43 @@ package notepad.presenter;
 import notepad.model.Notepad;
 import notepad.ui.NotepadUI;
 
+import java.io.IOException;
+import java.util.logging.*;
+
 
 public class Presenter {
     private final NotepadUI ui;
     private final Notepad notepad;
 
+    static Logger logger = Logger.getLogger(Presenter.class.getName());
+
+    static  {
+        ConsoleHandler cHandler = new ConsoleHandler();
+        SimpleFormatter formatter = new SimpleFormatter();
+        cHandler.setFormatter(formatter);
+
+        try {
+            FileHandler fHandler = new FileHandler("SortLogger.log", 2000, 5);
+            fHandler.setFormatter(formatter);
+            logger.addHandler(fHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        logger.addHandler(cHandler);
+        logger.setLevel(Level.INFO);
+        logger.setUseParentHandlers(false);
+    }
+
     public Presenter(NotepadUI ui) {
         this.ui = ui;
         this.notepad = new Notepad();
+        logger.log(Level.INFO, "Notepad application is started");
     }
 
 
     public void exitProgram() {
+        logger.log(Level.INFO, "Notepad application is finishing");
         System.exit(0);
     }
 
@@ -26,6 +51,7 @@ public class Presenter {
         String title = ui.getString("Enter a title for new note: ");
         String content = ui.newNote();
         notepad.add(title, content);
+        logger.log(Level.INFO, "New note was created");
     }
 
     public void findNote() {
@@ -36,11 +62,14 @@ public class Presenter {
         try {
             int index = Integer.parseInt(ui.getString("Enter index of the note to delete: "));
             notepad.delete(index);
-            ui.showMessage("Note " + index + " is deleted");
+            ui.showMessage("Note " + index + " was deleted");
+            logger.log(Level.WARNING, "Note " + index + " was deleted");
         } catch (NumberFormatException e) {
             ui.showMessage("Incorrect input!!! Index should be a number!");
+            logger.log(Level.WARNING, e.getMessage());
         } catch (IndexOutOfBoundsException e) {
             ui.showMessage("Note with such index not found!");
+            logger.log(Level.WARNING, e.getMessage());
         }
 
     }
